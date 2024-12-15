1c0e59891ca1c59293d8df97bf3ea56dce39c6a7
MargauxThw <36945000+MargauxThw@users.noreply.github.com>
2024-12-15 11:00:21 +1100

    Add visualisation code for Day 14 P2

--- 2024/14/p2_code.py
+++ 2024/14/p2_code.py
@@ -5,6 +5,19 @@ x_bound = 101
 y_bound = 103
 time = 100
 
+def visualise_robots(pos, i):
+    print(str(i) + ":")
+    map = [["" for x in range(x_bound)] for y in range(y_bound)]
+    for y in range(y_bound):
+        for x in range(x_bound):
+            # if x == x_bound // 2 or y == y_bound // 2:
+            #     map[y][x] = " "
+            if (x, y) in pos:
+                map[y][x] = "*"
+            else:
+                map[y][x] = "."
+        print("".join(map[y]))
+
 i = -1
 new_ends = set()
 while len(new_ends) != len(text):
@@ -16,5 +29,6 @@ while len(new_ends) != len(text):
         x_final = (pos[0] + vel[0] * i) % x_bound
         y_final = (pos[1] + vel[1] * i) % y_bound
         new_ends.add((x_final, y_final))
+    # visualise_robots(new_ends, i)
 
 print(i)
